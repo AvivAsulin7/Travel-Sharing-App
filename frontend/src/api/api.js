308b00a9d91aa9ac2d9717e2b30ea2078ece199a
אביב אסולין <avivasulin1211@gmail.com>
2023-03-03 10:10:21 +0200

    change the url-2

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://travel-app-ww2y.onrender.com";
+const url = "https://travel-app-backend-x2bg.onrender.com";
 // "http://localhost:5000"
 
 export const signUp = (user) => axios.post(`${url}/users/signup`, user);
